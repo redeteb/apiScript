e4b9216c7e8360c617a4abb69700a155e5141186
Ubuntu <ubuntu@ip-172-31-13-62.ec2.internal>
2024-08-28 20:29:28 +0000

    initial commit

---
+++ datescript.py
@@ -0,0 +1,28 @@
+
+menu = {
+    "Appetizers": {
+        "Breadsticks": {"Price": 10.0, "Ingredients": ["Bread", "Cheese", "Garlic"], "Dietary Restrictions": "GF/V"},
+        "Queso": {"Price": 11.0, "Ingredients": ["Cheddar", "Swiss", "Pepper Jack"], "Dietary Restrictions": "V"}, 
+        "Calamari": {"Price": 12.0, "Ingredients": ["Squid", "Panko", "Marinara"], "Dietary Restrictions": "GF"}
+    },
+    "Entree": {
+        "Chicken": {"Price": 16.0, "Ingredients": ["Chicken", "Mushrooms", "Pasta"], "Dietary Restrictions": "GF"},
+        "Beef": {"Price": 17.0, "Ingredients": ["Beef", "Onions"], "Dietary Restrictions": "GF"},
+        "Shrimp": {"Price": 20.0, "Ingredients": ["Squid", "Panko", "Marinara"], "Dietary Restrictions": "GF"},
+    },
+    "Dessert":{
+        "Cake": {"Price": 7.0, "Ingredients": ["Chocolate", "Sugar", "Milk"], "Dietary Restrictions": "GF/V"},
+        "Pie": {"Price": 6.0, "Ingredients": ["Cherries", "Bluesberries", "Rasberries"], "Dietary Restrictions": "GF"},
+        "Ice Cream": {"Price": 5.0, "Ingredients": ["Vanilla", "Chocolate", "Strawberry"], "Dietary Restrictions": "GF/V"},
+        },
+    "Beverages": {
+        "Tea": {"Price": 2.0, "Ingredients": ["Black", "Green", "Peach"], "Dietary Retrictions": "GF/V"}, 
+        "Coke": {"Price": 2.0, "Ingredients": ["Cup", "Ice", "Coke"], "Dietary Retrictions": "GF/V"},
+        "Water": {"Price": 1.0, "Ingredients": ["Cup", "Ice", "Water"], "Dietary Retrictions": "GF/V"},
+        }
+    }
+
+dateName = input("Hello! Welcome to our restaurant! We are happy to have you here. And I must ask, what is your date's name? They are stunning. ")
+print(dateName)
+
+
