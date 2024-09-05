ad82b2a8be128db11c36889bbf4e8252d5f17fea
Ubuntu <ubuntu@ip-172-31-13-62.ec2.internal>
2024-09-05 21:53:57 +0000

    Final Commit

--- apiCalls.py
+++ apiCalls.py
@@ -2,7 +2,7 @@ import requests
 
 url = "https://weatherbit-v1-mashape.p.rapidapi.com/current"
 
-querystring = {"lon":"9.0192","lat":"38.7525","units":"imperial","lang":"en"}
+querystring = {"lon":"20.0192","lat":"38.7525","units":"imperial","lang":"en"}
 
 headers = {
  	"x-rapidapi-key": "3d9c3c7db5msh063f33267a155e9p116340jsn6f42ca8e8b40",
@@ -13,9 +13,10 @@ response = requests.get(url, headers=headers, params=querystring)
 json_data = response.json()
 
 
-# print(response.json())
+city_name = json_data['data'][0]['city_name']
+country_name = json_data['data'][0]['country_code']
+temperature = json_data['data'][0]['temp']
 
-for key in json_data.keys():
-    print(key)
- 
+
+print(f"The weather in {city_name}, {country_name} is {temperature} Fahrenheit right now.")  
 
